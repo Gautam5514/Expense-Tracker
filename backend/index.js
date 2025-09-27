534a01693a3534b309bc9321cb76e0a5763e2d31
Gautam pandit <gautampandit797@gmail.com>
2025-09-28 01:12:41 +0530

    Babe

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000", "*"],
+  origin: ["http://localhost:5173", "http://localhost:3000", "https://expense-tracker-delta-green.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
